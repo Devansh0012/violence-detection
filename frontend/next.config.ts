3dd7595c8b491c1373897236fbc180a0a5d41f59
Devansh Dubey <107272625+Devansh0012@users.noreply.github.com>
2025-03-21 09:19:58 +0000

    updated config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     // Set NEXT_PUBLIC_API_URL to your Render URL in deployment.
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    NEXT_PUbLIC_WS_URL:
+      process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8000",
   },
 };
 
